997ab4e69d78070be280cefe196be5e0fe6f57db
Afnan-Navaz <afnannavaz@gmail.com>
2019-08-17 12:50:56 +0530

    add react-native-dependencies

---
+++ HFT/components/samplePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function SamplePage(){
+    return(
+        <View style={{justifyContent : 'center', alignItems : 'center', flex : 1 }}>
+            <Text>sample page</Text>
+        </View>
+    )
+}
+
+export default  SamplePage;
